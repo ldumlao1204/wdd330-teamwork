88f53e526d08e89191bd56d72b52cc1aa27fa789
Skriller22 <matthewremington@mac.com>
2026-05-14 22:42:05 -0600

    Part 1 of week 02 team assignment. Edits to product.js and tents.json, plus new files ProductDetails.mjs and product_pages/index.html.

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,11 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  let cartContents = getLocalStorage("so-cart") || [];
-  if (!Array.isArray(cartContents)) {
-    cartContents = [];
-  }
-  cartContents.push(product);
-  setLocalStorage("so-cart", cartContents);
-}
+console.log(dataSource.findProductById(productId));
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+await product.init();
