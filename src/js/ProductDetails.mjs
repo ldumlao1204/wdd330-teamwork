014c68efa5e1f6529a76432c4f89b155fc24099a
Skriller22 <matthewremington@mac.com>
2026-05-24 18:08:20 -0600

    Added cute little cart item counter to display the number of items in the cart using the length of the "so-cart" array.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,5 +1,5 @@
 // Purpose: This module is responsible for displaying the details of a single product. It retrieves the product data based on the product ID from the URL and renders it on the page.
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, renderCartCount } from "./utils.mjs";
 
 
 export default class ProductDetails {
@@ -80,6 +80,7 @@ export default class ProductDetails {
         }
         cartContents.push(this.product);
         setLocalStorage("so-cart", cartContents);
+        renderCartCount();
     }
 
 }
