009ac79c1097bbb0d76f965f64e7e568da177338
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-05-27 19:38:13 +0800

    Added discount to product detail pages task - updated  ProductList.mjs & style.css

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,9 +1,13 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+    const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
+    const discount = product.SuggestedRetailPrice - product.FinalPrice;
+    const discountPercent = Math.round((discount / product.SuggestedRetailPrice) * 100);
     return `<li class="product-card">
     <a href="/product_pages/?product=${product.Id}">
       <img src="${product.Images?.PrimaryMedium || ''}" alt="Image of ${product.Name}">
+      ${isDiscounted ? `<p class="product-card__discount-flag">${discountPercent}% off!</p>` : ''}
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.Name}</h3>
       <p class="product-card__price">$${product.FinalPrice}</p>
